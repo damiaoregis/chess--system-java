44b34a183f6d44e74feb5f523599e6e59e8a02a5
damiaoregis <damiao.v.serrana@gmail.com>
2026-03-18 14:47:16 -0300

    BoardException and defencive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -21,7 +21,7 @@ public class ChessMatch {
         }return mat;
     }
     private void initialSetup(){
-        board.placepiece(new Rook(board, Color.WHITE),new Position(1,1));
+        board.placepiece(new Rook(board, Color.WHITE),new Position(2,1));
         board.placepiece(new King(board, Color.BLACK),new Position(0,4));
         board.placepiece(new King(board, Color.WHITE),new Position(7,3));
 
